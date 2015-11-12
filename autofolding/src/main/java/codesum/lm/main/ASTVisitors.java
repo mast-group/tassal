3f0fa5f9b7c976bf0056f9dae34e954e5d7ce6b0
Jari <jaroslav.fowkes@ed.ac.uk>
2015-11-12 15:47:53 +0000

    Add support for folding fields

--- autofolding/src/main/java/codesum/lm/main/ASTVisitors.java
+++ autofolding/src/main/java/codesum/lm/main/ASTVisitors.java
@@ -19,27 +19,31 @@ import org.eclipse.jdt.core.dom.BodyDeclaration;
 import org.eclipse.jdt.core.dom.Comment;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.EnumDeclaration;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.ImportDeclaration;
 import org.eclipse.jdt.core.dom.Javadoc;
 import org.eclipse.jdt.core.dom.LineComment;
 import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.TagElement;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
-
-import codesum.lm.main.FoldableTree.FoldableNode;
-import codesum.lm.topicsum.GibbsSampler;
-import codesum.lm.vsm.TokenVector;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 import com.google.common.collect.DiscreteDomain;
 import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
 import com.google.common.collect.Multiset;
 import com.google.common.collect.Range;
 import com.google.common.collect.RangeSet;
 import com.google.common.collect.Table;
 import com.google.common.collect.TreeRangeSet;
+
+import codesum.lm.main.FoldableTree.FoldableNode;
+import codesum.lm.topicsum.GibbsSampler;
+import codesum.lm.vsm.TokenVector;
 
 public class ASTVisitors {
 
@@ -50,9 +54,8 @@ public class ASTVisitors {
 
 		/** List of ASTNode Types to fold */
 		protected static final HashSet<Integer> foldableTypes = new HashSet<Integer>(
-				Arrays.asList(ASTNode.COMPILATION_UNIT, ASTNode.BLOCK,
-						ASTNode.TYPE_DECLARATION, ASTNode.ENUM_DECLARATION,
-						ASTNode.JAVADOC));
+				Arrays.asList(ASTNode.COMPILATION_UNIT, ASTNode.BLOCK, ASTNode.TYPE_DECLARATION,
+						ASTNode.ENUM_DECLARATION, ASTNode.JAVADOC));
 
 		protected CompilationUnit cu;
 		protected FoldableTree tree;
@@ -60,14 +63,18 @@ public class ASTVisitors {
 
 		// Fold containers
 		public ArrayList<Range<Integer>> allFolds = Lists.newArrayList();
-		public HashMap<Range<Integer>, String> blockCommentFolds = Maps
-				.newHashMap();
-		public HashMap<Range<Integer>, String> lineCommentFolds = Maps
-				.newHashMap();
+		public HashMap<Range<Integer>, String> blockCommentFolds = Maps.newHashMap();
+		public HashMap<Range<Integer>, String> lineCommentFolds = Maps.newHashMap();
 		public HashMap<Range<Integer>, String> javadocFolds = Maps.newHashMap();
 
 		// Import container
 		public Range<Integer> importBlock;
+
+		// Field containers
+		public RangeSet<Integer> fieldLineRanges = TreeRangeSet.create();
+		final HashMap<Integer, ASTNode> fieldLineParentNodes = Maps.newHashMap();
+		final HashMap<Integer, Range<Integer>> lineToFieldRanges = Maps.newHashMap();
+		private final Multimap<Range<Integer>, String> fieldIdentifiers = HashMultimap.create();
 
 		// Settings containers
 		protected String fileString;
@@ -84,8 +91,7 @@ public class ASTVisitors {
 		// Shift start of class to after Javadoc
 		@Override
 		public boolean preVisit2(final ASTNode node) {
-			if (node.getNodeType() == ASTNode.TYPE_DECLARATION
-					|| node.getNodeType() == ASTNode.ENUM_DECLARATION)
+			if (node.getNodeType() == ASTNode.TYPE_DECLARATION || node.getNodeType() == ASTNode.ENUM_DECLARATION)
 				shiftStartPosition(node);
 			preVisit(node);
 			return true;
@@ -108,8 +114,7 @@ public class ASTVisitors {
 					String firstTagLine = "";
 					if (!jdoc.tags().isEmpty()) { // Handle empty comment
 
-						final String firstTag = ((TagElement) jdoc.tags()
-								.get(0)).toString();
+						final String firstTag = ((TagElement) jdoc.tags().get(0)).toString();
 						firstTagLine = firstTag.split("\n")[1].substring(2);
 
 						// If tokenizing comments add javadoc tokens to tree
@@ -122,6 +127,40 @@ public class ASTVisitors {
 				foldableStack.push(fn);
 				allFolds.add(fn.getRange());
 			}
+		}
+
+		// Looks for field declarations (i.e. class member variables)
+		@Override
+		public boolean visit(final FieldDeclaration node) {
+
+			// Shift start position to after Javadoc (if present)
+			shiftStartPosition(node);
+
+			// Add to relevant containers
+			final int line = cu.getLineNumber(node.getStartPosition());
+			fieldLineRanges.add(Range.singleton(line).canonical(DiscreteDomain.integers()));
+			fieldLineParentNodes.put(line, node.getParent());
+
+			final Range<Integer> range = Range.closed(node.getStartPosition(),
+					node.getStartPosition() + node.getLength() - 1);
+			lineToFieldRanges.put(line, range);
+
+			for (final Object fragment : node.fragments()) {
+				final VariableDeclarationFragment frag = (VariableDeclarationFragment) fragment;
+
+				// Split SIMPLE_NAME if token splitting enabled
+				final SimpleName name = frag.getName();
+				final ArrayList<String> identifiers = Lists.newArrayList();
+				putSplitToken(identifiers, name.getIdentifier());
+				for (final String identifier : identifiers)
+					fieldIdentifiers.put(range, identifier);
+
+				// Remove field identifiers from parent class
+				final FoldableNode parentClass = foldableStack.peek();
+				parentClass.removeTerms(identifiers);
+
+			}
+			return true;
 		}
 
 		@Override
@@ -148,8 +187,8 @@ public class ASTVisitors {
 				if (foldableStack.size() > 0) {
 					// Root class Javadoc outside of its associated class
 					if (node.getNodeType() == ASTNode.JAVADOC
-							&& (node.getParent().getNodeType() == ASTNode.TYPE_DECLARATION || node
-									.getParent().getNodeType() == ASTNode.ENUM_DECLARATION)) {
+							&& (node.getParent().getNodeType() == ASTNode.TYPE_DECLARATION
+									|| node.getParent().getNodeType() == ASTNode.ENUM_DECLARATION)) {
 						final FoldableNode classFn = foldableStack.pop();
 						foldableStack.peek().addChild(curFNode);
 						foldableStack.push(classFn);
@@ -169,15 +208,13 @@ public class ASTVisitors {
 				// Find location of Javadoc end */
 				final int nodeStart = node.getStartPosition();
 				final int nodeEnd = nodeStart + node.getLength() - 1;
-				final String nodeString = fileString.substring(nodeStart,
-						nodeEnd);
+				final String nodeString = fileString.substring(nodeStart, nodeEnd);
 				int jdocEndIdx = nodeString.indexOf("*/");
 
 				// Shift node start to next line
 				jdocEndIdx = nodeString.indexOf("\n", jdocEndIdx);
 				jdocEndIdx += 1;
-				node.setSourceRange(nodeStart + jdocEndIdx, nodeEnd - nodeStart
-						- jdocEndIdx + 1);
+				node.setSourceRange(nodeStart + jdocEndIdx, nodeEnd - nodeStart - jdocEndIdx + 1);
 
 			}
 
@@ -190,27 +227,22 @@ public class ASTVisitors {
 			// Handle multiple single-line comments
 			final RangeSet<Integer> lineRanges = TreeRangeSet.create();
 			final HashMap<Integer, ASTNode> lineParentNodes = Maps.newHashMap();
-			final HashMap<Integer, Range<Integer>> lineCommentRanges = Maps
-					.newHashMap();
+			final HashMap<Integer, Range<Integer>> lineCommentRanges = Maps.newHashMap();
 			final HashMap<Integer, String> lineComments = Maps.newHashMap();
 
 			// Get range (in chars) of comments
-			final Table<Comment, ASTNode, ArrayList<String>> commentNodes = HashBasedTable
-					.create();
+			final Table<Comment, ASTNode, ArrayList<String>> commentNodes = HashBasedTable.create();
 			for (final Comment node : (List<Comment>) cu.getCommentList()) {
 
 				// Extract block comments and add ranges to blockCommentFolds
 				// along with first non-empty line of comment
 				if (node.getNodeType() == ASTNode.BLOCK_COMMENT) {
 
-					final Range<Integer> range = Range.closed(
-							node.getStartPosition(), node.getStartPosition()
-									+ node.getLength() - 1);
-					final String commentText = fileString.substring(
-							range.lowerEndpoint(), range.upperEndpoint());
+					final Range<Integer> range = Range.closed(node.getStartPosition(),
+							node.getStartPosition() + node.getLength() - 1);
+					final String commentText = fileString.substring(range.lowerEndpoint(), range.upperEndpoint());
 					blockCommentFolds.put(range,
-							commentText.replaceAll("/\\*|\\*/", "").trim()
-									.split("\n")[0].replaceFirst("^\\*", ""));
+							commentText.replaceAll("/\\*|\\*/", "").trim().split("\n")[0].replaceFirst("^\\*", ""));
 					allFolds.add(range);
 
 					// If tokenizing comments add comment tokens to tree
@@ -222,32 +254,26 @@ public class ASTVisitors {
 				}
 
 				// Extract line comments
-				if (foldLineComments
-						&& node.getNodeType() == ASTNode.LINE_COMMENT) {
+				if (foldLineComments && node.getNodeType() == ASTNode.LINE_COMMENT) {
 
 					final int line = cu.getLineNumber(node.getStartPosition());
-					lineRanges.add(Range.singleton(line).canonical(
-							DiscreteDomain.integers()));
+					lineRanges.add(Range.singleton(line).canonical(DiscreteDomain.integers()));
 					lineParentNodes.put(line, getCoveringBlock(cu, node));
 
-					final Range<Integer> range = Range.closed(
-							node.getStartPosition(), node.getStartPosition()
-									+ node.getLength() - 1);
+					final Range<Integer> range = Range.closed(node.getStartPosition(),
+							node.getStartPosition() + node.getLength() - 1);
 					lineCommentRanges.put(line, range);
 
 					// Need to add one here or we lose the last character
-					final String commentText = fileString.substring(
-							range.lowerEndpoint(), range.upperEndpoint() + 1);
-					lineComments.put(line, commentText.replaceAll("//", "")
-							.trim());
+					final String commentText = fileString.substring(range.lowerEndpoint(), range.upperEndpoint() + 1);
+					lineComments.put(line, commentText.replaceAll("//", "").trim());
 				}
 
 			}
 
 			// Conflate multiple single line comments into one block comment
 			if (foldLineComments) {
-				conflate: for (final Range<Integer> lineRange : lineRanges
-						.asRanges()) {
+				conflate: for (final Range<Integer> lineRange : lineRanges.asRanges()) {
 
 					// Bizarrely RangeSet uses closedOpen ranges
 					final int startLine = lineRange.lowerEndpoint();
@@ -255,23 +281,20 @@ public class ASTVisitors {
 
 					// Ignore multiple one-line comments that are non-contiguous
 					for (int line = startLine; line < endLine; line++) {
-						final Integer endLineChar = lineCommentRanges.get(line)
-								.upperEndpoint() + 1; // else lose last char
-						final Integer startNextLineChar = lineCommentRanges
-								.get(line + 1).lowerEndpoint();
-						final String inbetweenText = fileString.substring(
-								endLineChar, startNextLineChar).trim();
+						final Integer endLineChar = lineCommentRanges.get(line).upperEndpoint() + 1; // else
+																										// lose
+																										// last
+																										// char
+						final Integer startNextLineChar = lineCommentRanges.get(line + 1).lowerEndpoint();
+						final String inbetweenText = fileString.substring(endLineChar, startNextLineChar).trim();
 						if (!inbetweenText.isEmpty())
 							continue conflate;
 					}
 
 					// Get character range for conflated comment
-					final Integer startChar = lineCommentRanges.get(startLine)
-							.lowerEndpoint();
-					final Integer endChar = lineCommentRanges.get(endLine)
-							.upperEndpoint();
-					final Range<Integer> range = Range.closed(startChar,
-							endChar);
+					final Integer startChar = lineCommentRanges.get(startLine).lowerEndpoint();
+					final Integer endChar = lineCommentRanges.get(endLine).upperEndpoint();
+					final Range<Integer> range = Range.closed(startChar, endChar);
 
 					// Get first non-empty line of comment text
 					String commentLine = "";
@@ -302,35 +325,69 @@ public class ASTVisitors {
 			tree.addNodes(commentNodes);
 		}
 
+		private void addFieldstoTree() {
+
+			if (!fieldLineRanges.isEmpty()) {
+
+				final Table<FieldDeclaration, ASTNode, ArrayList<String>> fieldNodes = HashBasedTable.create();
+
+				// Conflate field node ranges
+				for (final Range<Integer> lineRange : fieldLineRanges.asRanges()) {
+
+					// Bizarrely RangeSet uses closedOpen ranges
+					final int startLine = lineRange.lowerEndpoint();
+					final int endLine = lineRange.upperEndpoint() - 1;
+
+					// Add ranges to allFolds along with first
+					// non-empty line of comment
+					final Integer startChar = lineToFieldRanges.get(startLine).lowerEndpoint();
+					final Integer endChar = lineToFieldRanges.get(endLine).upperEndpoint();
+					final Range<Integer> conflatedRange = Range.closed(startChar, endChar);
+					allFolds.add(conflatedRange);
+
+					// Get tokens in conflated range
+					final ArrayList<String> tokens = Lists.newArrayList();
+					for (final Range<Integer> range : fieldIdentifiers.keySet()) {
+						if (conflatedRange.encloses(range))
+							tokens.addAll(fieldIdentifiers.get(range));
+					}
+
+					// Create single block node and add tokens
+					final VariableDeclarationFragment fragment = cu.getAST().newVariableDeclarationFragment();
+					final FieldDeclaration node = cu.getAST().newFieldDeclaration(fragment);
+					final ASTNode parent = fieldLineParentNodes.get(startLine);
+					node.setSourceRange(startChar, endChar - startChar + 1);
+					fieldNodes.put(node, parent, tokens);
+				}
+
+				// Add conflated field nodes to tree
+				tree.addNodes(fieldNodes);
+			}
+		}
+
 		/** Add imports as single node to foldable tree */
 		protected void addImportsTree() {
 
 			@SuppressWarnings("unchecked")
-			final List<ImportDeclaration> imports = (List<ImportDeclaration>) cu
-					.imports();
+			final List<ImportDeclaration> imports = (List<ImportDeclaration>) cu.imports();
 			if (!imports.isEmpty()) {
 
 				// Get range of imports (starting after first import statement)
 				final ImportDeclaration firstImport = imports.get(0);
-				final int startChar = firstImport.getStartPosition()
-						+ firstImport.getLength() - 1;
-				final ImportDeclaration lastImport = imports
-						.get(imports.size() - 1);
-				final int endChar = lastImport.getStartPosition()
-						+ lastImport.getLength() - 1;
+				final int startChar = firstImport.getStartPosition() + firstImport.getLength() - 1;
+				final ImportDeclaration lastImport = imports.get(imports.size() - 1);
+				final int endChar = lastImport.getStartPosition() + lastImport.getLength() - 1;
 
 				// Add import identifiers to identifier list
 				final List<String> importIdentifiers = Lists.newArrayList();
 				for (final ImportDeclaration importNode : imports)
-					putTokenizedImports(importIdentifiers, importNode.getName()
-							.getFullyQualifiedName());
+					putTokenizedImports(importIdentifiers, importNode.getName().getFullyQualifiedName());
 
 				// Remove import identifiers from root token list
 				tree.getRoot().removeTerms(importIdentifiers);
 
 				// Add imports as single node to tree
-				final ImportDeclaration node = cu.getAST()
-						.newImportDeclaration();
+				final ImportDeclaration node = cu.getAST().newImportDeclaration();
 				node.setSourceRange(startChar, endChar - startChar + 1);
 				final FoldableNode fn = tree.new FoldableNode(node);
 				fn.addTerms(importIdentifiers);
@@ -343,16 +400,16 @@ public class ASTVisitors {
 		}
 
 		/** Tokenize imports */
-		private void putTokenizedImports(final List<String> importIdentifiers,
-				final String importString) {
+		private void putTokenizedImports(final List<String> importIdentifiers, final String importString) {
 
 			for (final String token : importString.split("\\."))
 				putSplitToken(importIdentifiers, token);
 		}
 
-		/** Tokenize comment String into words (split each word and lowercase) */
-		public ArrayList<String> tokenizeCommentString(
-				final String commentString) {
+		/**
+		 * Tokenize comment String into words (split each word and lowercase)
+		 */
+		public ArrayList<String> tokenizeCommentString(final String commentString) {
 
 			final ArrayList<String> identifierList = Lists.newArrayList();
 			for (final String token : commentString.split("\\W+")) {
@@ -363,16 +420,15 @@ public class ASTVisitors {
 		}
 
 		/** Split on CamelCase and _under_score if splitting tokens */
-		protected void putSplitToken(final List<String> identifierList,
-				final String token) {
+		protected void putSplitToken(final List<String> identifierList, final String token) {
 			if (splitTokens)
 				putTokenParts(identifierList, token);
 			else
 				identifierList.add(token);
 		}
 
-		public void process(final CompilationUnit unit, final File file,
-				final TokenVector fv, final GibbsSampler smp, final Settings set) {
+		public void process(final CompilationUnit unit, final File file, final TokenVector fv, final GibbsSampler smp,
+				final Settings set) {
 
 			// Read source file to string
 			fileString = CodeUtils.readFileString(file);
@@ -387,9 +443,10 @@ public class ASTVisitors {
 			cu = unit;
 			unit.accept(this);
 
-			// Add comments and imports to tree and set levels
+			// Add comments, imports and fields to tree and set levels
 			addCommentsTree();
 			addImportsTree();
+			addFieldstoTree();
 			tree.setLevels();
 
 			// Set budget
@@ -501,12 +558,13 @@ public class ASTVisitors {
 		}
 	}
 
-	/** Get covering Block/Class node, returning the root node if there is none */
-	public static ASTNode getCoveringBlock(final CompilationUnit root,
-			final ASTNode node) {
+	/**
+	 * Get covering Block/Class node, returning the root node if there is none
+	 */
+	public static ASTNode getCoveringBlock(final CompilationUnit root, final ASTNode node) {
 
-		final CoveringBlockFinderVisitor finder = new CoveringBlockFinderVisitor(
-				node.getStartPosition(), node.getLength());
+		final CoveringBlockFinderVisitor finder = new CoveringBlockFinderVisitor(node.getStartPosition(),
+				node.getLength());
 		root.accept(finder);
 		final ASTNode coveringBlock = finder.getCoveringBlock();
 
@@ -523,10 +581,8 @@ public class ASTVisitors {
 	 * @see {@link codemining.java.codeutils.IdentifierTokenRetriever#putTokenParts}
 	 * @author Jaroslav Fowkes
 	 */
-	public static void putTokenParts(final List<String> identifierList,
-			final String identifier) {
-		for (final String token : identifier
-				.split("((?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z]))|_")) {
+	public static void putTokenParts(final List<String> identifierList, final String identifier) {
+		for (final String token : identifier.split("((?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z]))|_")) {
 			if (!token.equals(""))
 				identifierList.add(token.toLowerCase());
 		}
